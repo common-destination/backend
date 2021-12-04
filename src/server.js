2abaac5e653d67d4a7904de462368b0923e93480
pierluigi <pierluigibaiano@gmail.com>
2021-12-04 16:38:00 +0100

    TEST

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import './config.js';
+import './db-connect.js';
+import express from 'express';
+import cors from 'cors';
+import { flightRouter } from './routers/flightRouter.js';
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/', flightRouter, (req, res) => {
+  res.status(404).send({
+    message: "404 page not found",
+    url: req.originalUrl,
+  });
+});
+app.listen(port, () => console.log(`http://localhost:${port}`));
+//test
