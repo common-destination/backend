1d0034e23dae2734e5a8a9ffb8c8cb063c32ca22
giacomotolari <tolarigiacomo@gmail.com>
2022-01-10 11:24:58 +0100

    create getRange function

--- src/data/airports.js
+++ src/data/airports.js
@@ -1,178 +1,186 @@
-export const airports = [
-    {
-      name: 'London',
-      country: 'UK',
-      range: 8,
-      passengersInMillionPerYear: 80,
-    },
-    {
-      name: 'Paris',
-      country: 'France',
-      range: 8,
-      passengersInMillionPerYear: 76,
-    },
-    {
-      name: 'Amsterdam',
-      country: 'Netherlands',
-      range: 7,
-      passengersInMillionPerYear: 71,
-    },
-    {
-      name: 'Frankfurt',
-      country: 'Germany',
-      range: 7,
-      passengersInMillionPerYear: 70,
-    },
-    {
-      name: 'Barcelona',
-      country: 'Spain',
-      range: 5,
-      passengersInMillionPerYear: 52,
-    },
-    {
-      name: 'Istanbul',
-      country: 'Turkey',
-      range: 5,
-      passengersInMillionPerYear: 52,
-    },
-    {
-      name: 'Moscow',
-      country: 'Russia',
-      range: 5,
-      passengersInMillionPerYear: 49,
-    },
-    {
-      name: 'Freising',
-      country: 'Germany',
-      range: 5,
-      passengersInMillionPerYear: 47,
-    },
-    {
-      name: 'Rome',
-      country: 'Italy',
-      passengersInMillionPerYear: 43,
-      range: 4,
-    },
-    {
-      name: 'Dublin',
-      country: 'Ireland',
-      passengersInMillionPerYear: 32,
-      range: 3,
-    },
-    {
-      name: 'Vienna',
-      country: 'Austria',
-      passengersInMillionPerYear: 31,
-      range: 3,
-    },
-    {
-      name: 'Zurich',
-      country: 'Switzerland',
-      passengersInMillionPerYear: 31,
-      range: 3,
-    },
-    {
-      name: 'Lisbon',
-      country: 'Portugal',
-      passengersInMillionPerYear: 31,
-      range: 3,
-    },
-    {
-      name: 'Copenhagen',
-      country: 'Denmark',
-      passengersInMillionPerYear: 30,
-      range: 3,
-    },
-    {
-      name: 'Palma de Mallorca',
-      country: 'Spain',
-      range: 3,
-      passengersInMillionPerYear: 29,
-    },
-    {
-      name: 'Milan',
-      country: 'Italy',
-      range: 3,
-      passengersInMillionPerYear: 28,
-    },
-    {
-      name: 'Oslo',
-      country: 'Norway',
-      range: 3,
-      passengersInMillionPerYear: 28,
-    },
-    {
-      name: 'Brussels',
-      country: 'Belgium',
-      range: 3,
-      passengersInMillionPerYear: 26,
-    },
-    {
-      name: 'Stockholm',
-      country: 'Sweden',
-      range: 2,
-      passengersInMillionPerYear: 25,
-    },
-    {
-      name: 'Athens',
-      country: 'Greece',
-      range: 2,
-      passengersInMillionPerYear: 25,
-    },
-    {
-      name: 'Dusseldorf',
-      country: 'Germany',
-      range: 2,
-      passengersInMillionPerYear: 25,
-    },
-    {
-      name: 'Berlin',
-      country: 'Germany',
-      range: 2,
-      passengersInMillionPerYear: 24,
-    },
-    {
-      name: 'Helsinki',
-      country: 'Finland',
-      range: 2,
-      passengersInMillionPerYear: 21,
-    },
-    {
-      name: 'Saint Petersburg',
-      country: 'Russia',
-      range: 2,
-      passengersInMillionPerYear: 19,
-    },
-    {
-      name: 'Warsaw',
-      country: 'Poland',
-      range: 2,
-      passengersInMillionPerYear: 18,
-    },
-    {
-      name: 'Prague',
-      country: 'Czech Republic',
-      range: 2,
-      passengersInMillionPerYear: 17,
-    },
-    {
-      name: 'Hamburg',
-      country: 'Germany',
-      range: 2,
-      passengersInMillionPerYear: 17,
-    },
-    {
-      name: 'Budapest',
-      country: 'Hungary',
-      range: 1,
-      passengersInMillionPerYear: 16,
-    },
-    {
-      name: 'Stuttgart',
-      country: 'Germany',
-      range: 1,
-      passengersInMillionPerYear: 12,
-    },
-  ];
+const getRange = (howManyPassengers) => {
+  return Math.round(howManyPassengers);
+};
 
-  
+const getRange = (passengers) => {
+  return Math.round(passengers / 10);
+};
+
+
+export const airports = [
+  {
+    name: 'London',
+    country: 'UK',
+    passengersInMillionPerYear: 80,
+    range: getRange(passengersInMillionPerYear),
+  },
+  {
+    name: 'Paris',
+    country: 'France',
+    range: 8,
+    passengersInMillionPerYear: 76,
+  },
+  {
+    name: 'Amsterdam',
+    country: 'Netherlands',
+    range: 7,
+    passengersInMillionPerYear: 71,
+  },
+  {
+    name: 'Frankfurt',
+    country: 'Germany',
+    range: 7,
+    passengersInMillionPerYear: 70,
+  },
+  {
+    name: 'Barcelona',
+    country: 'Spain',
+    range: 5,
+    passengersInMillionPerYear: 52,
+  },
+  {
+    name: 'Istanbul',
+    country: 'Turkey',
+    range: 5,
+    passengersInMillionPerYear: 52,
+  },
+  {
+    name: 'Moscow',
+    country: 'Russia',
+    range: 5,
+    passengersInMillionPerYear: 49,
+  },
+  {
+    name: 'Freising',
+    country: 'Germany',
+    range: 5,
+    passengersInMillionPerYear: 47,
+  },
+  {
+    name: 'Rome',
+    country: 'Italy',
+    passengersInMillionPerYear: 43,
+    range: 4,
+  },
+  {
+    name: 'Dublin',
+    country: 'Ireland',
+    passengersInMillionPerYear: 32,
+    range: 3,
+  },
+  {
+    name: 'Vienna',
+    country: 'Austria',
+    passengersInMillionPerYear: 31,
+    range: 3,
+  },
+  {
+    name: 'Zurich',
+    country: 'Switzerland',
+    passengersInMillionPerYear: 31,
+    range: 3,
+  },
+  {
+    name: 'Lisbon',
+    country: 'Portugal',
+    passengersInMillionPerYear: 31,
+    range: 3,
+  },
+  {
+    name: 'Copenhagen',
+    country: 'Denmark',
+    passengersInMillionPerYear: 30,
+    range: 3,
+  },
+  {
+    name: 'Palma de Mallorca',
+    country: 'Spain',
+    range: 3,
+    passengersInMillionPerYear: 29,
+  },
+  {
+    name: 'Milan',
+    country: 'Italy',
+    range: 3,
+    passengersInMillionPerYear: 28,
+  },
+  {
+    name: 'Oslo',
+    country: 'Norway',
+    range: 3,
+    passengersInMillionPerYear: 28,
+  },
+  {
+    name: 'Brussels',
+    country: 'Belgium',
+    range: 3,
+    passengersInMillionPerYear: 26,
+  },
+  {
+    name: 'Stockholm',
+    country: 'Sweden',
+    range: 2,
+    passengersInMillionPerYear: 25,
+  },
+  {
+    name: 'Athens',
+    country: 'Greece',
+    range: 2,
+    passengersInMillionPerYear: 25,
+  },
+  {
+    name: 'Dusseldorf',
+    country: 'Germany',
+    range: 2,
+    passengersInMillionPerYear: 25,
+  },
+  {
+    name: 'Berlin',
+    country: 'Germany',
+    range: 2,
+    passengersInMillionPerYear: 24,
+  },
+  {
+    name: 'Helsinki',
+    country: 'Finland',
+    range: 2,
+    passengersInMillionPerYear: 21,
+  },
+  {
+    name: 'Saint Petersburg',
+    country: 'Russia',
+    range: 2,
+    passengersInMillionPerYear: 19,
+  },
+  {
+    name: 'Warsaw',
+    country: 'Poland',
+    range: 2,
+    passengersInMillionPerYear: 18,
+  },
+  {
+    name: 'Prague',
+    country: 'Czech Republic',
+    range: 2,
+    passengersInMillionPerYear: 17,
+  },
+  {
+    name: 'Hamburg',
+    country: 'Germany',
+    range: 2,
+    passengersInMillionPerYear: 17,
+  },
+  {
+    name: 'Budapest',
+    country: 'Hungary',
+    range: 1,
+    passengersInMillionPerYear: 16,
+  },
+  {
+    name: 'Stuttgart',
+    country: 'Germany',
+    range: 1,
+    passengersInMillionPerYear: 12,
+  },
+];
+
