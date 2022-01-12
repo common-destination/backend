a69126b7d70003cec4e7ae9f3e8c99a26a445af5
Khambazarov <khambazarovrenat@gmail.com>
2022-01-12 13:14:57 +0100

    update seed

--- src/seed.js
+++ src/seed.js
@@ -1,17 +1,16 @@
-import './config.js';
-import './db-connect.js';
-import mongoose from 'mongoose';
+import "./config.js";
+import "./db-connect.js";
+import mongoose from "mongoose";
 import FlightModel from "./models/flightModel.js";
-// import flights from "../src/fakeDatas/flights.js"
-import getFlightsAPI from "./APIGenerator/api.js"
+import getFlightsAPI from "./APIGenerator/api.js";
 
 // clear dummy data
-// await FlightModel.deleteMany({})
+// await FlightModel.deleteMany({});
 
 // SEED flights first
 const flightsDb = await FlightModel.create(await getFlightsAPI());
-// const flightsDb = await FlightModel.create(flights);
 
+// const flightsDb = await FlightModel.create(flights);
 console.log(flightsDb);
 
 mongoose.connection.close(); // close connection and quit script
