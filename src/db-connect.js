81de8916de70df1f82493a225caba7fef5e16c18
Pierluigi10 <79852750+Pierluigi10@users.noreply.github.com>
2022-01-12 11:32:39 +0100

    Revert "Validations (#7)"
    This reverts commit d82e2a2f1daf7462a9c0229060f4c420947b69f8.

--- src/db-connect.js
+++ src/db-connect.js
@@ -1,18 +1,17 @@
-import "./config.js";
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 // URINNEN
 // SENSITIVE DATA => never store stuff like this in code
 // connection strings, passwords, secrets
 
 // we need dotenv.config() to be executred BEFOE!
-// const mongoConnectionString = "mongodb://localhost/common_destination";
-const mongoConnectionString = process.env.MONGO_ATLAS;
+const mongoConnectionString = process.env.MONGO_URI;
+console.log({ mongoConnectionString});
 
 mongoose
   .connect(mongoConnectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB Connection estabished!"))
-  .catch((err) => console.log("[ERROR] Connection failed", err));
+  .then(() => console.log('DB Connection estabished!'))
+  .catch((err) => console.log('[ERROR] Connection failed', err));
--- src/seed.js
+++ src/seed.js
@@ -2,15 +2,13 @@ import './config.js';
 import './db-connect.js';
 import mongoose from 'mongoose';
 import FlightModel from "./models/flightModel.js";
-// import flights from "../src/fakeDatas/flights.js"
-import getFlightsAPI from "./APIGenerator/api.js"
+import flights from "../src/fakeDatas/flights.js"
 
 // clear dummy data
-// await FlightModel.deleteMany({})
+await FlightModel.deleteMany({})
 
 // SEED flights first
-const flightsDb = await FlightModel.create(await getFlightsAPI());
-// const flightsDb = await FlightModel.create(flights);
+const flightsDb = await FlightModel.create(flights);
 
 console.log(flightsDb);
 
