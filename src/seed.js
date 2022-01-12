865a0ac0e1ad6ea51b1c18bc69741790f9be9d16
Khambazarov <khambazarovrenat@gmail.com>
2022-01-12 15:04:13 +0100

    change foldernames singular to plural

--- src/seed.js
+++ src/seed.js
@@ -1,19 +1,16 @@
 import "./config.js";
 import "./db-connect.js";
 import mongoose from "mongoose";
-import FlightModel from "./models/flightModel.js";
+import FlightsModel from "./models/flightsModel.js";
 
-// import flights from "../src/fakeDatas/flights.js"
 import getFlightsAPI from "./APIGenerator/api.js"
 
 // clear dummy data
-// await FlightModel.deleteMany({})
+// await FlightsModel.deleteMany({})
 
 // SEED flights first
-const flightsDb = await FlightModel.create(await getFlightsAPI());
-// const flightsDb = await FlightModel.create(flights);
+const flightsDb = await FlightsModel.create(await getFlightsAPI());
 
-// const flightsDb = await FlightModel.create(flights);
 console.log(flightsDb);
 
 mongoose.connection.close(); // close connection and quit script
