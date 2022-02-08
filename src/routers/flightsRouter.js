7eda9180c33b97aaa9bdc99f7ea4713ee7f29d09
giacomotolari <63161008+giacomotolari@users.noreply.github.com>
2022-02-08 23:16:51 +0100

    create common-destination router and controller (#22)
    Co-authored-by: giacomotolari <tolarigiacomo@gmail.com>

--- src/routers/flightsRouter.js
+++ src/routers/flightsRouter.js
@@ -169,48 +169,7 @@ flightsRouter.get("/descending-departuredate", async (req, res) => {
   res.json(getSorteDate);
 });
 
-//COMPATIBLE FLIGHTS FOR EVERY PASSENGER
 
-flightsRouter.get("/compatible-flights", async (req, res) => {
-  const passengers = [
-    {
-      name: "passenger2",
-      airport: "Hamburg",
-      minDepartureDate: moment("2022-02-01 08:02"),
-      maxReturnDate: moment("2022-02-04 08:02"),
-    },
-  ];
-  const flights = await flightsController.compatibleFlights(passengers);
-  res.json(flights);
-});
-
-//GET COMMON DESTINATION D
-
-flightsRouter.post("/passengers-data", async (req, res) => {
-  let passengers = req.body.passengers;
-  let stayTimeTogether = req.body.stayTimeTogether;
-  req.session.passengers = passengers;
-  req.session.stayTimeTogether = stayTimeTogether;
-  req.session.save();
-
-  // const minStayTimeTogether = 2;
-  // time together = smallest return - largest arrival
-  // const flights = await flightsController.findCommonDestinations(passengers);
-  // res.json(flights);
-  res.json(passengers);
-});
-
-flightsRouter.get("/common-destination", async (req, res) => {
-  let passengers = req.session.passengers;
-  let stayTimeTogether = req.session.stayTimeTogether;
-  console.log(passengers);
-  console.log(stayTimeTogether);
-
-
-
-  
-  res.json(passengers);
-});
 
 // GET ALL FLIGHTS
 flightsRouter.get("/", async (_req, res) => {
